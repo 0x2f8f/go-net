3e1da88a074b91d7dd4e2cbbbf54205cf85fa481
0x2f8f <0x2f8f@gmail.com>
2019-03-25 00:22:46 +0300

    templates

--- main.go
+++ main.go
@@ -3,20 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 )
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templ, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	templ.ExecuteTemplate(w, "index", nil)
 }
 
 func infoHandler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "info page")
+	fmt.Fprintf(w, "<h1>info page</h1>")
 }
 
 func main() {
 	fmt.Println("Listening on port :3000")
 
-	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/info", infoHandler)
 
 	http.ListenAndServe(":3000", nil)

8c5706cf4d194ad081b3234f56efbe4c107f6404
0x2f8f <0x2f8f@gmail.com>
2019-03-31 00:28:57 +0300

    create page

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templ, err := template.ParseFiles("templates/index.html")
+	templ, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -22,11 +22,43 @@ func infoHandler(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "<h1>info page</h1>")
 }
 
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	t.ExecuteTemplate(w, "write", nil)
+}
+/*
+func savePostHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+
+	var post *models.Post
+	if id != "" {
+		post = posts[id]
+		post.Title = title
+		post.Content = content
+	} else {
+		id = GenerateId()
+		post := models.NewPost(id, title, content)
+		posts[post.Id] = post
+	}
+
+	http.Redirect(w, r, "/", 302)
+}
+*/
 func main() {
 	fmt.Println("Listening on port :3000")
 
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/info", infoHandler)
+	http.HandleFunc("/write", writeHandler)
+	//http.HandleFunc("/SavePost", savePostHandler)
 
 	http.ListenAndServe(":3000", nil)
 
